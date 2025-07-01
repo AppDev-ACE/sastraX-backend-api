14f76e3a0004b5d53034896b02791893f8ac57a4
AppDev-ACE <unify.service.team@gmail.com>
2025-07-01 12:14:20 +0530

    Added captcha and login

--- captcha.js
+++ captcha.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+// To provide captcha to the UI
 app.get('/captcha', async (req, res) => {
     const browser = await puppeteer.launch({
       headless: true,
@@ -17,13 +18,46 @@ app.get('/captcha', async (req, res) => {
     await page.goto("https://webstream.sastra.edu/sastrapwi/");
     await new Promise(resolve => setTimeout(resolve, 1500));
     await page.waitForSelector('#imgCaptcha', { timeout: 5000 });
-    const captchaPath = '/tmp/captcha.png';
+    const captchaPath = path.join(__dirname, 'captcha.png');
     const captchaElement = await page.$('#imgCaptcha');
     await captchaElement.screenshot({ path: captchaPath });
 
     await browser.close();
     res.sendFile(captchaPath);
 });
+
+// To get the reg no, password and captcha from the user and login
+app.post('/login', async (req, res) => {
+  const { regno, pwd, captcha } = req.body;
+  
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+
+    const page = await browser.newPage();
+    await page.goto("https://webstream.sastra.edu/sastrapwi/");
+
+    await page.type("#txtRegNumber", regno);
+    await page.type("#txtPwd", pwd);
+    await page.type("#answer", captcha);
+
+    await Promise.all([
+      page.click('input[type="submit"]'),
+      page.waitForNavigation({ waitUntil: 'networkidle0' })
+    ]);
+
+    const loginFailed = await page.$('.ui-state-error');
+    if (loginFailed) {
+      const msg = await page.evaluate(el => el.textContent.trim(), loginFailed);
+      await browser.close();
+      return res.status(401).json({ success: false, message: msg });
+    }
+
+    await browser.close();
+    return res.json({ success: true, message: "Login successful!" });
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
